2fbdb899807cdf8287d3c62c89e653ce0e9e4932
TheJana125 <jtheierova@gmail.com>
2022-05-15 18:43:32 +0200

    Ukol4-hotovo

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -4,44 +4,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Třída pro losování Sportky.
- */
+
 public class Sportka {
   private final List<Integer> osudi = new ArrayList<>();
 
   public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-  }
+      for (int i = 0; i < 50; i++) {
+        osudi.add(i);
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
+  }
+  }
   public void zamichat() {
-    //TODO zamíchat osudí
+    Collections.shuffle(osudi);
+
   }
 
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
   public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
+    return osudi.subList(0, 6);
   }
 
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
+
   public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return null;
+    return osudi.get(7);
+
   }
 }
